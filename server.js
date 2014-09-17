59bf83b9cd58b88b9798f34f0c27b84cf0af85d6
Sampo Tolvanen <sampo.tolvanen@gmail.com>
2014-09-17 12:40:48 +0300

    Add application configuration file
    * Use configurations from config file if the file exists

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  * TIE-13106 (TIE-13100) Project Work on Pervasive Systems
  */
 
+var fs = require('fs');
 var express = require('./node_modules/express');
 var routes = require('./routes');
 var http = require('http');
@@ -11,8 +12,15 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+// Load config if exists
+var config = {};
+var configFile = './config.js';
+if (fs.existsSync(configFile)) {
+    config = require(configFile);
+}
+
 // Envinronment settings
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (config.port || process.env.PORT || 5000));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
